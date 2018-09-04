8a6bb2f2b24f6fc28a3f2614fad83e7bad16afe7
Jiahao Zhang <jiahaoz@edison03.nersc.gov>
2018-09-04 13:45:58 -0700

    first

---
+++ interface.cpp
@@ -0,0 +1,9 @@
+#include <fstream>
+#include <string>
+double** getparameter(std::string pair_style,std::fstream fs){
+	std::string line;
+	do{
+		getline(fs,line);
+
+	}while(line);
+}
---
+++ interface.h
@@ -0,0 +1,6 @@
+#ifndef interface_h
+#define interface_h
+#include <fstream>
+#include <string>
+double** getparameter(std::string pair_style,std::fstream fs)
+#endif
