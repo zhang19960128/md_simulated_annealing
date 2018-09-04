20cddbb85b3111cbc1ed429992d23f4bc222fc45
Jiahao Zhang <jiahaoz@edison03.nersc.gov>
2018-09-04 14:25:19 -0700

    first

--- interface.cpp
+++ interface.cpp
@@ -1,9 +1,18 @@
 #include <fstream>
 #include <string>
+#include <sstream>
 double** getparameter(std::string pair_style,std::fstream fs){
 	std::string line;
+	istringstream stream1;
+	std::string temp;
 	do{
 		getline(fs,line);
-
+		stream1.str(line);
+		stream1 >> temp;
+		if(temp=="pair_coeff"){
+			std::cout<<"Ok I found it"<<std::endl;
+		}
 	}while(line);
+	double** p;
+	return p;
 }
