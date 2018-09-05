0799da0965cc137e2aad884923881059ef28263c
Jiahao Zhang <jiahaoz@edison03.nersc.gov>
2018-09-05 11:27:45 -0700

    fixed the construct image problems

--- image.cpp
+++ image.cpp
@@ -16,24 +16,23 @@ atom* imageall(atom* input,int size,double* p,double cutoff,int& virt_size){
         std::copy(input,input+size,allimage+i*size);
 	}
 	int shiftv[3]={0,0,0};
-	int imagetick=0;/*this specify the image you need to shift*/
-	for(int i=-1*virt_size;i<=virt_size;i++)
-		for(int j=-1*virt_size;j<=virt_size;j++)
-			for(int k=-1*virt_size;k<=virt_size;k++){
+	int imagetick=0;
+	for(int k=-1*nimage;k<=nimage;k++)
+		for(int j=-1*nimage;j<=nimage;j++)
+			for(int i=-1*nimage;i<=nimage;i++){
 				shiftv[0]=i;
 				shiftv[1]=j;
 				shiftv[2]=k;
-				imagetick=(i+virt_size)+(j+virt_size)*virt_size+(k+virt_size)*virt_size*virt_size;
-				shift(allimage+imagetick,size,p,shiftv);
+				imagetick=(i+nimage)+(j+nimage)*virt_size+(k+nimage)*virt_size*virt_size;
+				shift(allimage+imagetick*size,size,p,shiftv);
 	}
 	virt_size=virt_size*virt_size*virt_size*size;
-	std::cout<<virt_size<<std::endl;
 	return allimage;
 }
 void shift(atom* input,int size,double* p,int* shiftv){
 	for(int i=0;i<size;i++){
 		for(size_t j=0;j<3;j++){
-			(input+i)->position[j]=(input+i)->position[j]+shiftv[j]*p[i];
+			(input+i)->position[j]=(input+i)->position[j]+shiftv[j]*p[j];
 		}
 	}
 }
