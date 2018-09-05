40b60c44208f0588d01abdbc29c8b698987a4da6
Jiahao Zhang <jiahaoz@edison03.nersc.gov>
2018-09-05 09:14:44 -0700

    add destruction files

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@
 int main(){
    double** inputbv=getparameter("bv","in.BTO");
 	 double** inputbvv=getparameter("bvv","in.BTO");
+	 double** inputljcoul=getparameter("12lj/cut/coul/long","in.BTO");
 	 /*input the configuration for lammps simulation*/
 	 atom* testconfig;//the test atom configuration for bond-valence model in lammps
 	 testconfig=configuration("mixdata.BTO");
 	 double period[3]={40.4,40.4,40.4};
 	 box test(testconfig,4,40,period,inputbv,inputbvv);
+	 test.freezeforce();
+	 test.
 	 return 0;
 }
