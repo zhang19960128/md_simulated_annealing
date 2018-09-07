6ea89e01da349f806f6111b5f6c8cf2c714a2453
zjh19960128 <zjh199601283492@gmail.com>
2018-09-06 22:05:24 -0400

    first

--- bvv.cpp
+++ bvv.cpp
@@ -15,7 +15,7 @@ void box::updatelistbvv(){
 			temp=distance(allatom[i].position,virtatom[j].position);
             paircut=bvvrcut[allatom[i].type][virtatom[j].type];
             if(temp<paircut && temp>0.0000001){
-				allatom[i].neibv.push_back(j);
+				allatom[i].neibvv.push_back(j);
 			}
 		}
 	}
@@ -25,10 +25,15 @@ void box::updatebvv(double** pairbvv_input){
 	for(size_t i=0;i<type;i++)
 		for(size_t j=i;j<type;j++){
 			r0[i][j]=pairbvv_input[temp][0];
+                        r0[j][i]=r0[i][j];
 			cij[i][j]=pairbvv_input[temp][1];
+                        r0[j][i]=r0[i][j];
 			svvij[i][j]=pairbvv_input[temp][2];
-			v0[i][j]=pairbvv_input[temp][3];
+                        svvij[j][i]=svvij[i][j];
+			vv0[i][j]=pairbvv_input[temp][3];
+                        vv0[j][i]=v0[i][j];
 			bvvrcut[i][j]=pairbvv_input[temp][4];
+                        bvvrcut[j][i]=bvvrcut[i][j];
 			temp++;
 		}
 }
@@ -45,26 +50,29 @@ void box::computebvv(){
 double delx,dely,delz,rsq,recip,r,s,ss;
 	bvvenergy=0.00;
 	double* fp=new double[size];
+        double temp;
 	for(size_t i=0;i<size;i++){
-		allatom[i].s0=0;
 		allatom[i].s0x=0.0;
 		allatom[i].s0y=0.0;
 		allatom[i].s0z=0.0;
-		for(std::list<int>::iterator j=allatom[i].neibv.begin();j!=allatom[i].neibv.end();j++){
+		for(std::list<int>::iterator j=allatom[i].neibvv.begin();j!=allatom[i].neibvv.end();j++){
 			delx=allatom[i].position[0]-virtatom[*j].position[0];
 			dely=allatom[i].position[1]-virtatom[*j].position[1];
 			delz=allatom[i].position[2]-virtatom[*j].position[2];
 			rsq=delx*delx+dely*dely+delz*delz;
 			r=sqrt(rsq);
 			recip=1.0/r;
-			allatom[i].s0+=pow(r0[allatom[i].type][virtatom[*j].type]/r,cij[allatom[i].type][virtatom[*j].type]);
-			allatom[i].s0x+=allatom[i].s0*delx/r;
-			allatom[i].s0y+=allatom[i].s0*dely/r;
-			allatom[i].s0z+=allatom[i].s0*delz/r;
+			temp=pow(r0[allatom[i].type][virtatom[*j].type]/r,cij[allatom[i].type][virtatom[*j].type]);
+			allatom[i].s0x+=temp*delx/r;
+			allatom[i].s0y+=temp*dely/r;
+			allatom[i].s0z+=temp*delz/r;
 		}
-		s=allatom[i].s0x*allatom[i].s0x+allatom[i].s0y+allatom[i].s0y+allatom[i].s0z*allatom[i].s0z-vv0[allatom[i].type][allatom[i].type];
+		s=allatom[i].s0x*allatom[i].s0x+allatom[i].s0y+allatom[i].s0y+allatom[i].s0z*allatom[i].s0z-vv0[allatom[i].type][allatom[i].type]*vv0[allatom[i].type][allatom[i].type];
 		ss=s*s;
 		bvvenergy=bvvenergy+svvij[allatom[i].type][allatom[i].type]*ss;
 	}
-	std::cout<<"bond valence energy is: "<<bvvenergy<<std::endl;
+        for(size_t i=0;i<4;i++){
+            std::cout<<svvij[i][0]<<" "<<svvij[i][1]<<" "<<svvij[i][2]<<" "<<svvij[i][3]<<std::endl;
+        }
+	std::cout<<"the all bond valence energy is: "<<bvvenergy+bvenergy<<std::endl;
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -std=c++11 -o out main.cpp interface.cpp image.cpp atom.cpp
+g++ -std=c++11 -o out main.cpp interface.cpp image.cpp atom.cpp bv.cpp bvv.cpp
 ./out
