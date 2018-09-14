030b1ebe5b3e1cb0f5452506142fe66fbe60125b
Jiahao Zhang <jiahaoz@edison05.nersc.gov>
2018-09-13 20:21:20 -0700

    speed up ewald_jiahaoz

--- ewald_jiahaoz.cpp
+++ ewald_jiahaoz.cpp
@@ -16,7 +16,9 @@ void box::computelong(){
 		/*e/(epsilon0*A)=180.951  ev when use U=k*q1*q2/r*/
     double volume=p[0]*p[1]*p[2];
     double delx,dely,delz,rsq,r;
-    double root2pi=pow(2*pi,0.5);
+    double root2pi=sqrt(2*pi);
+		double root2=sqrt(2);
+		double rootpi=sqrt(pi);
     double* xall=new double[size];
     double* yall=new double [size];
     double* zall=new double [size];
@@ -24,6 +26,7 @@ void box::computelong(){
 		double* fx=new double [size];
 		double* fy=new double [size];
 		double* fz=new double [size];
+		double chargei,chargej,temp,temp2,r3;
     for(size_t i=0;i<size;i++){
        xall[i]=allatom[i].position[0];
        yall[i]=allatom[i].position[1];
@@ -32,21 +35,34 @@ void box::computelong(){
 			 fy[i]=0.00;
 			 fz[i]=0.00;
        allcharge[i]=allatom[i].charge;
+			 chargei=allcharge[i];
        for(std::list<int>::iterator j=allatom[i].neilj.begin();j!=allatom[i].neilj.end();j++){
-         delx=allatom[i].position[0]-virtatom[*j].position[0];
+         chargej=virtatom[*j].charge;
+				 delx=allatom[i].position[0]-virtatom[*j].position[0];
          dely=allatom[i].position[1]-virtatom[*j].position[1];
          delz=allatom[i].position[2]-virtatom[*j].position[2];
          rsq=delx*delx+dely*dely+delz*delz;
          r=sqrt(rsq);
-         ShortRange+=1/epsil/4/pi*allatom[i].charge*virtatom[*j].charge/r*erfc(r/sqrt(2)/sigma);
-      }
+				 r3=r*rsq;
+				 temp=erfc(r/root2/sigma);
+         ShortRange+=1/epsil/4/pi*chargei*chargej/r*temp;
+				 temp2=1.0/8.0/epsil/pi*chargei*chargej/r3*erfc(r/root2/sigma);
+				 fx[i]=delx*temp2+fx[i];
+				 fy[i]=dely*temp2+fy[i];
+				 fz[i]=delz*temp2+fz[i];
+				 temp=1.0/4/pi/epsil*1.0/2.0*chargei*chargej/rsq*(1.0/rootpi*2.0*exp(-1*rsq/2.0/sigma/sigma))*1.0/root2/sigma;
+				 fx[i]=fx[i]+temp*delx;
+				 fy[i]=fy[i]+temp*dely;
+				 fz[i]=fz[i]+temp*delz;
+			 }
+			// std::cout<<fx[i]<<" "<<fy[i]<<" "<<fz[i]<<std::endl;
     }
     ShortRange=ShortRange/2.0;
-		std::cout<<"the short range coulumb potential is: "<<std::setprecision(10)<<ShortRange<<std::endl;
+		//std::cout<<"the short range coulumb potential is: "<<std::setprecision(10)<<ShortRange<<std::endl;
     for(size_t i=0;i<size;i++){
       selfe=selfe-1/root2pi/sigma/4/pi/epsil*(allatom[i].charge)*allatom[i].charge;
     }
-		std::cout<<"the self energy is: "<<selfe<<std::endl;
+		//std::cout<<"the self energy is: "<<selfe<<std::endl;
     /*calculate the longrange energy*/
     /*exp(i*k_v*r)
      *=exp(i*k_x*r_x)*exp(i*k_y*r_y)*exp(i*k_z*r_z)
@@ -80,7 +96,10 @@ void box::computelong(){
 			//		std::cout<<cskxrx[i][j]<<" "<<cskyry[i][j]<<" "<<cskzrz[i][j]<<std::endl;
 				  //std::cout<<xall[j]<<" "<<yall[j]<<" "<<zall[j]<<std::endl;
        }
-    double skre,skim,skmodsq,ksq;
+    double skre,skim,skmodsq,ksq,snkr,cskr,kr,sumh,sumk,suml;
+		sumh=0.0;
+		sumk=0.0;
+		suml=0.0;
     for(int h=-1*gmax;h<=gmax;h++)
        for(int k=-1*gmax;k<=gmax;k++)
           for(int l=-1*gmax;l<=gmax;l++){
@@ -100,14 +119,40 @@ void box::computelong(){
 	        skim=skim+allcharge[i]*snkxrx[abs(h)][i]*cskyry[abs(k)][i]*cskzrz[abs(l)][i]*(h>0 ? 1:-1);
 	        skim=skim+allcharge[i]*cskxrx[abs(h)][i]*snkyry[abs(k)][i]*cskzrz[abs(l)][i]*(k>0 ? 1:-1);
 	        skim=skim-allcharge[i]*snkxrx[abs(h)][i]*snkyry[abs(k)][i]*snkzrz[abs(l)][i]*(h>0 ? 1:-1)*(k>0 ? 1:-1)*(l>0 ? 1:-1);
-             }
+						 }
+          ksq=h*2*pi/p[0]*h*2*pi/p[0]+k*2*pi/p[1]*k*2*pi/p[1]+l*2*pi/p[2]*l*2*pi/p[2];
+		      skmodsq=skre*skre+skim*skim;
+		      temp=exp(-1*sigma*sigma*ksq/2)/ksq;
+		      LongRange=LongRange+1/volume/2/epsil*temp*skmodsq;
+						 for(size_t i=0;i<size;i++){
 		//std::cout<<"the real part is: "<<skre<<" the imaginary part is: "<<skim<<std::endl;
-		 ksq=h*2*pi/p[0]*h*2*pi/p[0]+k*2*pi/p[1]*k*2*pi/p[1]+l*2*pi/p[2]*l*2*pi/p[2];
-		 skmodsq=skre*skre+skim*skim;
-		 LongRange=LongRange+1/volume/2/epsil*exp(-1*sigma*sigma*ksq/2)/ksq*skmodsq;
-          }
+		 			chargei=allcharge[i];
+		 			//kr=h*2*pi/p[0]*xall[i]+k*2*pi/p[1]*yall[i]+l*2*pi/p[2]*zall[i];
+					//snkr=sin(kr);
+					//snkr=(snkxrx[abs(h)][i]*cskyry[abs(k)][i])*cskzrz[abs(l)][i]*(h>0?1:-1);
+					snkr=(snkxrx[abs(h)][i]*cskyry[abs(k)][i]*(h>0?1:-1)+cskxrx[abs(h)][i]*snkyry[abs(k)][i]*(k>0?1:-1))*cskzrz[abs(l)][i];
+					/*
+					 * the full formula require this, but due to symmetry, the net sum of snkzrz is basically zeros, so this is useless
+					snkr=snkr+(cskxrx[abs(h)][i]*cskyry[abs(k)][i]-snkxrx[abs(h)][i]*snkyry[abs(k)][i]*(h>0?1:-1)*(k>0?1:-1))*snkzrz[abs(l)][i];
+		 			*/
+					//cskr=cos(kr);
+				  //	cskr=cskxrx[abs(h)][i]*cskyry[abs(k)][i]*cskzrz[abs(l)][i];
+					  	cskr=(cskxrx[abs(h)][i]*cskyry[abs(k)][i]-snkxrx[abs(h)][i]*snkyry[abs(k)][i]*(h>0?1:-1)*(k>0?1:-1))*cskzrz[abs(l)][i];
+				 /*
+				 * the full formula require this, but due to symmetry, the net sum of snkzrz is basically zeros, so this is useless.
+					cskr=cskr-(snkxrx[abs(h)][i]*cskyry[abs(k)][i]*(h>0?1:-1)+cskxrx[abs(h)][i]*snkyry[abs(k)][i]*(k>0?1:-1))*snkzrz[abs(l)][i];
+         
+				 */
+					fx[i]=fx[i]+1/volume/2/epsil*temp*chargei*2*pi/p[0]*h*2*(snkr*skre-cskr*skim);
+					fy[i]=fy[i]+1/volume/2/epsil*temp*chargei*2*pi/p[1]*k*2*(snkr*skre-cskr*skim);
+					fz[i]=fz[i]+1/volume/2/epsil*temp*chargei*2*pi/p[2]*l*2*(snkr*skre-cskr*skim);
+						 }
+						 }
 		std::cout<<"the long range energy is: "<<LongRange<<std::endl;
 		std::cout<<"the toatl energy is: "<<std::setprecision(10)<<std::setw(10)<<LongRange+selfe+ShortRange<<std::endl;
+		for(size_t i=0;i<size;i++){
+			std::cout<<fx[i]<<" "<<fy[i]<<" "<<fz[i]<<std::endl;
+		}
 		/*this is the final step*/
     for(size_t i=0;i<gmax;i++){
       delete [] cskxrx[i];
