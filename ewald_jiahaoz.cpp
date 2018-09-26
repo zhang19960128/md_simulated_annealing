308ea57da8ec503f5b377e5d9cccd996613fe71a
Jiahao Zhang <jiahaoz@cori09.nersc.gov>
2018-09-26 08:21:33 -0700

    fix short range problem

--- ewald_jiahaoz.cpp
+++ ewald_jiahaoz.cpp
@@ -6,6 +6,13 @@
 #include <iomanip>
 #define pi 3.14159265359
 /*Perform the Ewald summation. The lattice paramters should be given*/
+#define EWALD_F   1.12837917//actually this is 2/sqrt(3)
+#define EWALD_P   0.3275911
+#define A1        0.254829592
+#define A2       -0.284496736
+#define A3        1.421413741
+#define A4       -1.453152027
+#define A5        1.061405429
 /*Lattice should be a flattened array*/
 void box::computelong(){
     double ShortRange = 0;
@@ -13,6 +20,7 @@ void box::computelong(){
     double selfe = 0;
     double epsil = 0.0055263885;
 		double coul_prefactor=180.9512801302711;
+		double ewald_alpha=1/sqrt(2)/sigma;
 		/*e/(epsilon0*A)=180.951  ev when use U=k*q1*q2/r*/
     double volume=p[0]*p[1]*p[2];
     double delx,dely,delz,rsq,r;
@@ -26,7 +34,7 @@ void box::computelong(){
 		double* fx=new double [size];
 		double* fy=new double [size];
 		double* fz=new double [size];
-		double chargei,chargej,temp,temp2,r3;
+		double chargei,chargej,temp,temp2,r3,erfc_interpolate,expm2,grij,t;//erfc_exact; use to debug when compare different erfc function.
     for(size_t i=0;i<size;i++){
        xall[i]=allatom[i].position[0];
        yall[i]=allatom[i].position[1];
@@ -44,18 +52,26 @@ void box::computelong(){
          rsq=delx*delx+dely*dely+delz*delz;
          r=sqrt(rsq);
 				 r3=r*rsq;
-				 temp=erfc(r/root2/sigma);
-         ShortRange+=1/epsil/4/pi*chargei*chargej/r*temp;
-				 temp2=1.0/8.0/epsil/pi*chargei*chargej/r3*erfc(r/root2/sigma);
+				// temp=erfc(r/root2/sigma);
+        // ShortRange+=1/epsil/4/pi*chargei*chargej/r*temp;
+				 //temp2=1.0/8.0/epsil/pi*chargei*chargej/r3*erfc(r/root2/sigma); this is the standard erfc function but now I want to use the interpolation method to accelerate.
+				 grij=ewald_alpha*r;
+				 expm2=exp(-grij*grij);//=exp(-1*rsq/2/sigma^2)
+				 t= 1.0 / (1.0 + EWALD_P*grij);
+				 erfc_interpolate = t * (A1+t*(A2+t*(A3+t*(A4+t*A5)))) * expm2;
+				 //std::cout<<"the difference is: "<<erfc_interpolate-erfc(r/root2/sigma)<<std::endl;
+				 ShortRange+=1/epsil/4/pi*chargei*chargej/r*erfc_interpolate;
+				 temp2=1.0/8.0/epsil/pi*chargei*chargej/r3*erfc_interpolate;
 				 fx[i]=delx*temp2+fx[i];
 				 fy[i]=dely*temp2+fy[i];
 				 fz[i]=delz*temp2+fz[i];
-				 temp=1.0/4/pi/epsil*1.0/2.0*chargei*chargej/rsq*(1.0/rootpi*2.0*exp(-1*rsq/2.0/sigma/sigma))*1.0/root2/sigma;
+				 //temp=1.0/4/pi/epsil*1.0/2.0*chargei*chargej/rsq*(1.0/rootpi*2.0*expm2)*1.0/root2/sigma;
+				 temp=1.0/4/pi/epsil*1.0/2.0*chargei*chargej/r3*1.0/rootpi*2.0*(EWALD_F*expm2*grij+erfc_interpolate);
 				 fx[i]=fx[i]+temp*delx;
 				 fy[i]=fy[i]+temp*dely;
 				 fz[i]=fz[i]+temp*delz;
 			 }
-			// std::cout<<fx[i]<<" "<<fy[i]<<" "<<fz[i]<<std::endl;
+			 std::cout<<fx[i]<<" "<<fy[i]<<" "<<fz[i]<<std::endl;
     }
     ShortRange=ShortRange/2.0;
 		//std::cout<<"the short range coulumb potential is: "<<std::setprecision(10)<<ShortRange<<std::endl;
@@ -134,16 +150,17 @@ void box::computelong(){
 					snkr=snkr+(cskxrx[abs(h)][i]*cskyry[abs(k)][i]-snkxrx[abs(h)][i]*snkyry[abs(k)][i]*(h>0?1:-1)*(k>0?1:-1))*snkzrz[abs(l)][i];
 		 			*/
 					cskr=cos(kr);
-				  	cskr=cskxrx[abs(h)][i]*cskyry[abs(k)][i]*cskzrz[abs(l)][i];
-					  cskr=(cskxrx[abs(h)][i]*cskyry[abs(k)][i]-snkxrx[abs(h)][i]*snkyry[abs(k)][i]*(h>0?1:-1)*(k>0?1:-1))*cskzrz[abs(l)][i];
-						cskr=cskr-(snkxrx[abs(h)][i]*cskyry[abs(k)][i]*(h>0?1:-1)+cskxrx[abs(h)][i]*snkyry[abs(k)][i]*(k>0?1:-1))*snkzrz[abs(l)][i];
+				  //	cskr=cskxrx[abs(h)][i]*cskyry[abs(k)][i]*cskzrz[abs(l)][i];
+					//  cskr=(cskxrx[abs(h)][i]*cskyry[abs(k)][i]-snkxrx[abs(h)][i]*snkyry[abs(k)][i]*(h>0?1:-1)*(k>0?1:-1))*cskzrz[abs(l)][i];
+					//	cskr=cskr-(snkxrx[abs(h)][i]*cskyry[abs(k)][i]*(h>0?1:-1)+cskxrx[abs(h)][i]*snkyry[abs(k)][i]*(k>0?1:-1))*snkzrz[abs(l)][i];
 					fx[i]=fx[i]+1/volume/2/epsil*temp*chargei*2*pi/p[0]*h*2*(snkr*skre-cskr*skim);
 					    fy[i]=fy[i]+1/volume/2/epsil*temp*chargei*2*pi/p[1]*k*2*(snkr*skre-cskr*skim);
 					    fz[i]=fz[i]+1/volume/2/epsil*temp*chargei*2*pi/p[2]*l*2*(snkr*skre-cskr*skim);
 						 }
 						 }
-		std::cout<<"the long range energy is: "<<LongRange<<std::endl;
-		std::cout<<"the self energy is: "<<selfe<<std::endl;
+		std::cout<<"the long range energy is: "<<std::setprecision(10)<<std::setw(10)<<LongRange<<std::endl;
+		std::cout<<"the self energy is: "<<std::setprecision(10)<<std::setw(10)<<selfe<<std::endl;
+		std::cout<<"the Short Range energy is: "<<std::setprecision(10)<<std::setw(10)<<ShortRange<<std::endl;
 		std::cout<<"the toatl energy is: "<<std::setprecision(10)<<std::setw(10)<<LongRange+selfe+ShortRange<<std::endl;
 		for(size_t i=0;i<size;i++){
 			std::cout<<fx[i]<<" "<<fy[i]<<" "<<fz[i]<<std::endl;
